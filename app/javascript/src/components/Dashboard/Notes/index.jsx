5b3361a25ec985f5aa54f2666f12eb140d5e9afe
shamilsdq <shamilsdq@live.com>
2022-01-17 16:21:50 +0530

    Added SideMenu and TopHeader for Notes page

--- app/javascript/src/components/Dashboard/Notes/index.jsx
+++ app/javascript/src/components/Dashboard/Notes/index.jsx
@@ -1,22 +1,17 @@
 import React, { useState, useEffect } from "react";
 
 import EmptyNotesListImage from "images/EmptyNotesList";
-import { Button, PageLoader } from "neetoui/v2";
-import { Container, Header, SubHeader } from "neetoui/v2/layouts";
+import { PageLoader } from "neetoui/v2";
 
 import notesApi from "apis/notes";
 import EmptyState from "components/Common/EmptyState";
 
-import DeleteAlert from "./DeleteAlert";
-import NoteTable from "./NoteTable";
-import NewNotePane from "./Pane/CreateNote";
+import SideMenu from "./SideMenu";
+import TopHeader from "./TopHeader";
 
 const Notes = () => {
   const [loading, setLoading] = useState(true);
-  const [showNewNotePane, setShowNewNotePane] = useState(false);
-  const [showDeleteAlert, setShowDeleteAlert] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [selectedNoteIds, setSelectedNoteIds] = useState([]);
+  const [showSideMenu, setShowSideMenu] = useState(true);
   const [notes, setNotes] = useState([]);
 
   useEffect(() => {
@@ -40,59 +35,22 @@ const Notes = () => {
   }
 
   return (
-    <Container>
-      <Header
-        title="Notes"
-        actionBlock={
-          <Button
-            onClick={() => setShowNewNotePane(true)}
-            label="Add New Note"
-            icon="ri-add-line"
-          />
-        }
-      />
+    <div className="flex w-full">
+      <SideMenu isOpen={showSideMenu} />
       {notes.length ? (
-        <>
-          <SubHeader
-            searchProps={{
-              value: searchTerm,
-              onChange: e => setSearchTerm(e.target.value),
-              clear: () => setSearchTerm(""),
-            }}
-            deleteButtonProps={{
-              onClick: () => setShowDeleteAlert(true),
-              disabled: !selectedNoteIds.length,
-            }}
-          />
-          <NoteTable
-            setSelectedNoteIds={setSelectedNoteIds}
-            notes={notes}
-            fetchNotes={fetchNotes}
-          />
-        </>
+        <div className="flex flex-col w-full px-5">
+          <TopHeader toggleSideMenu={() => setShowSideMenu(prev => !prev)} />
+        </div>
       ) : (
         <EmptyState
           image={EmptyNotesListImage}
           title="Looks like you don't have any notes!"
           subtitle="Add your notes to send customized emails to them."
-          primaryAction={() => setShowNewNotePane(true)}
+          primaryAction={() => {}}
           primaryActionLabel="Add New Note"
         />
       )}
-      <NewNotePane
-        showPane={showNewNotePane}
-        setShowPane={setShowNewNotePane}
-        fetchNotes={fetchNotes}
-      />
-      {showDeleteAlert && (
-        <DeleteAlert
-          selectedNoteIds={selectedNoteIds}
-          onClose={() => setShowDeleteAlert(false)}
-          refetch={fetchNotes}
-          setSelectedNoteIds={setSelectedNoteIds}
-        />
-      )}
-    </Container>
+    </div>
   );
 };
 
