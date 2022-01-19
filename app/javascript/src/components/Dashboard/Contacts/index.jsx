2695a132ae86586a0541386813ec7658747b4403
shamilsdq <shamilsdq@live.com>
2022-01-19 18:46:58 +0530

    Created a simple Contacts component and add it to route

---
+++ app/javascript/src/components/Dashboard/Contacts/index.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+
+import EmptyNotesListImage from "images/EmptyNotesList";
+import { PageLoader } from "neetoui/v2";
+
+import EmptyState from "components/Common/EmptyState";
+
+const Notes = () => {
+  const [loading, setLoading] = useState(true);
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetchContacts();
+  }, []);
+
+  const fetchContacts = async () => {
+    try {
+      setLoading(true);
+      setContacts([]);
+    } catch (error) {
+      logger.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return <PageLoader />;
+  }
+
+  return (
+    <div className="flex w-full">
+      {contacts.length ? (
+        <div className="flex flex-col w-full px-5"></div>
+      ) : (
+        <EmptyState
+          image={EmptyNotesListImage}
+          title="Looks like you don't have any contacts!"
+          subtitle="Add your contacts to send customized emails to them."
+          primaryAction={() => {}}
+          primaryActionLabel="Add New Note"
+        />
+      )}
+    </div>
+  );
+};
+
+export default Notes;
