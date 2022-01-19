17b1db1760733447f484f907707460fc81acdb0e
shamilsdq <shamilsdq@live.com>
2022-01-19 14:27:08 +0530

    Added content to pane

--- app/javascript/src/components/Dashboard/Notes/FormPane/index.jsx
+++ app/javascript/src/components/Dashboard/Notes/FormPane/index.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 
-import { Pane, Typography } from "neetoui/v2";
+import { Check } from "@bigbinary/neeto-icons";
+import { Pane, Typography, Button } from "neetoui/v2";
+
+import CreateForm from "./CreateForm";
 
 const FormPane = ({ isOpen, close }) => (
   <Pane isOpen={isOpen} onClose={close}>
     <Pane.Header>
-      <Typography style="h2" weight="semibold">
+      <Typography style="h2" weight="semibold" className="px-2">
         Add New Note
       </Typography>
     </Pane.Header>
+
+    <Pane.Body>
+      <CreateForm />
+    </Pane.Body>
+
+    <Pane.Footer>
+      <Button label="Save Changes" size="large" icon={Check} onClick={close} />
+      <Button
+        label="Cancel"
+        style="text"
+        size="large"
+        onClick={close}
+        className="ml-3"
+      />
+    </Pane.Footer>
   </Pane>
 );
 
