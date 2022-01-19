245a65ad9668e322407cb55da2c3ad13b8cdb9e6
shamilsdq <shamilsdq@live.com>
2022-01-19 13:37:08 +0530

    Updated size of alert

--- app/javascript/src/components/Dashboard/Notes/DeleteAlert.jsx
+++ app/javascript/src/components/Dashboard/Notes/DeleteAlert.jsx
@@ -6,6 +6,7 @@ const DeleteAlert = ({ isOpen, close }) => (
   <Alert
     title="Delete Note"
     message="Are you sure you want to delete the note? This cannot be undone."
+    size="md"
     isOpen={isOpen}
     onSubmit={close}
     onClose={close}
