848c22bb3aaf29bce7b5fd6ffd4727b253f9830d
shamilsdq <shamilsdq@live.com>
2022-01-24 23:15:02 +0530

    Fixed styling for input elements

--- app/javascript/src/components/Dashboard/Notes/Form.jsx
+++ app/javascript/src/components/Dashboard/Notes/Form.jsx
@@ -5,26 +5,29 @@ import { Input, Textarea, Select } from "neetoui/v2";
 const Form = () => (
   <form className="w-full px-2 space-y-6">
     <Input
+      required
       label="Title"
       name="title"
       placeholder="Enter note title"
+      size="large"
       className="flex-grow-0 w-full"
-      required
     />
     <Textarea
+      required
       label="Description"
       name="description"
       placeholder="Enter note description"
+      size="large"
       className="flex-grow-0 w-full"
       rows={2}
-      required
     />
     <Select
-      label="Assigned Contact"
-      placeholder="Select Role"
       required
-    ></Select>
-    <Select label="Tags" placeholder="Select Role" required></Select>
+      label="Assigned Contact"
+      name="contact"
+      placeholder="Select Role"
+    />
+    <Select required label="Tags" name="tags" placeholder="Select Role" />
   </form>
 );
 
